65193e57347e62d7483ba8afe606d7b76c8c7f1e
dir300 <67864121+dir300@users.noreply.github.com>
2025-03-06 16:19:17 +0400

    Update scripr.js

--- scripr.js
+++ scripr.js
@@ -1,7 +1,6 @@
 const citySelect = document.getElementById('citySelect');
 const timeDisplay = document.getElementById('time');
 
-// Функция для обновления времени
 function updateTime() {
     const timeZone = citySelect.value;
     const now = new Date();
@@ -9,11 +8,6 @@ function updateTime() {
     timeDisplay.textContent = `Текущее время: ${timeString}`;
 }
 
-// Обновляем время при изменении выбора города
 citySelect.addEventListener('change', updateTime);
-
-// Обновляем время каждую секунду
 setInterval(updateTime, 1000);
-
-// Инициализация времени при загрузке страницы
 updateTime();
